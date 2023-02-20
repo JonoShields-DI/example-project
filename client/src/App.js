4d165b2e975b70e91c821927667a9c25c27410b8
Jono Shields <jono.shields@developersinstitute.ac.nz>
2023-02-21 12:36:45 +1300

    Add fetch to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const get_example = async() => {
+    const response = await fetch('http://localhost:5001/',
+      {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: "GET"
+      }
+    )
+    console.log(await response.json());
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button onClick={get_example}>GET</button>
   );
 }
 
