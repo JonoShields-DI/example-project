346bb1101a860a1185d741a2e6a2ce1fa851d9ae
Jono Shields <jono.shields@developersinstitute.ac.nz>
2023-02-21 14:02:48 +1300

    Add database call to api to add product

--- api/app.js
+++ api/app.js
@@ -1,35 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const db = require('./db');
 
-var indexRouter = require('./routes/index');
-
-var app = express();
-
-app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+const product = { name : "product1", description: 'This is a description'}
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/test', function(req, res, next) {
+  res.json({ title: 'Express' });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.post('/product', function(req, res, next) {
+  const product = req.body;
+  console.log(req.body)
+  db.query(`INSERT INTO example ("name", "description") values ($1, $2)`, [product.name, product.description])
+  res.send(201);
 });
 
-module.exports = app;
+app.get('/product', function(req, res, next) {
+  res.json({ title: 'Express' });
+});
+
+app.listen(5001, () => {
+  console.log(`Example app listening at http://localhost:${5001}`);
+});
