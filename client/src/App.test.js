4a909c9a240689fe47c17018951fface16b9a4d8
Jono Shields <jono.shields@developersinstitute.ac.nz>
2023-02-22 13:59:20 +1300

    Add test for two buttons

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders application title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/test application/i);
+  expect(headingElement).toBeInTheDocument();
 });
+
+test('renders 2 buttons', () => {
+  render(<App />);
+  const button1 = screen.getByRole('button', {name : /get/i});
+  expect(button1).toBeInTheDocument();
+  const button2 = screen.getByRole('button', {name: /post/i});
+  expect(button2).toBeInTheDocument();
+});
+
